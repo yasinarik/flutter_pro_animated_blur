101c916851b531d5c6a3f545d4b4054c4f4eacd4
yasinarik <35217388+yasinarik@users.noreply.github.com>
2022-02-08 18:50:53 +0300

    pubspec.yaml updated

--- lib/pro_animated_blur.dart
+++ lib/pro_animated_blur.dart
@@ -1,3 +1,8 @@
+/// This package is created by Yasin Arık.
+///   - Email: yasin.ariky@gmail.com
+/// Github repo of the package:
+///   - https://github.com/yasinarik/flutter_pro_animated_blur
+
 library pro_animated_blur;
 
 export 'src/pro_animated_blur.dart';
